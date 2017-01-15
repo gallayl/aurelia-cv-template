3e87d777bed7ea8f8711a557277f1d272f7108c0
Gallay Lajos <gallay.lajos@live.com>
2017-01-15 12:35:17 +0100

    Add: Materialize, Dist task into separate folder

--- aurelia_project/tasks/build.ts
+++ aurelia_project/tasks/build.ts
@@ -3,6 +3,7 @@ import transpile from './transpile';
 import processMarkup from './process-markup';
 import processCSS from './process-css';
 import {build} from 'aurelia-cli';
+import {dist} from './dist';
 import * as project from '../aurelia.json';
 
 export default gulp.series(
@@ -12,7 +13,8 @@ export default gulp.series(
     processMarkup,
     processCSS
   ),
-  writeBundles
+  writeBundles,
+  dist
 );
 
 function readProjectConfiguration() {
